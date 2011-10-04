657648451e3d87f578fe89385331c9ed1e45fc23
dotrakoun <dotrakoun@arn.com>
2011-10-04 16:30:01 -0400

    forced addition of prod config since it's gitignored. oops

---
+++ wp-config-prod.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Production server configuration
+ */
+define('SERVER_ENVIRONMENT', 'PROD');
+define('WP_DEBUG', false);
+
+define('DB_NAME', 'name');
+define('DB_USER', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
