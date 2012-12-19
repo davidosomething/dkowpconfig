a9d17332719133e10005631eb16d0d3a49c853b3
David O'Trakoun <me@davidosomething.com>
2012-12-19 13:16:35 -0500

    remove root uploads folder, remove wp-conftentdir setting in common-before, add plugins dir to commo-after

--- config/common-after.php
+++ config/common-after.php
@@ -4,14 +4,19 @@
  * This file is included AFTER the environment specific config
  */
 
-if (!defined('WP_CONTENT_DIR')) define('WP_CONTENT_DIR', DKO_SITE_DIR . '/content');
+// Site URLs -- these should be manually set for Production
+if (!defined('WP_HOME')) define('WP_HOME',    DKO_CONFIG_PROTOCOL . '//' . $_SERVER['SERVER_NAME']); // path to blog root
+if (!defined('WP_SITEURL')) define('WP_SITEURL', WP_HOME . '/wp'); // path to wordpress
+
+if (!defined('WP_CONTENT_DIR')) define('WP_CONTENT_DIR', dirname(__DIR__) . '/content');
 if (!defined('WP_CONTENT_URL')) define('WP_CONTENT_URL', WP_HOME . '/content');
 
-if (!defined('WP_PLUGIN_DIR')) define('WP_PLUGIN_DIR', DKO_SITE_DIR . '/plugins');
+if (!defined('WP_PLUGIN_DIR')) define('WP_PLUGIN_DIR', dirname(__DIR__) . '/plugins');
 if (!defined('WP_PLUGIN_URL')) define('WP_PLUGIN_URL', WP_HOME . '/plugins');
 
-// Uploads dir is stored in database as a CMS setting
+if (!defined('WPMU_PLUGIN_URL')) define('WPMU_PLUGIN_URL', dirname(__DIR__) . '/mu-plugins');
+if (!defined('WPMU_PLUGIN_DIR')) define('WPMU_PLUGIN_DIR', WP_HOME . '/mu-plugins');
 
 define('WPLANG', '');
-if (!defined('ABSPATH')) define('ABSPATH', dirname(__FILE__) . '/wp/');
+if (!defined('ABSPATH')) define('ABSPATH', __DIR__ . '/wp/');
 require_once(ABSPATH . 'wp-settings.php');
--- config/common-before.php
+++ config/common-before.php
@@ -3,6 +3,3 @@
  * DKO WordPress Configuration file
  * This file is included BEFORE the environment specific config
  */
-
-if (!defined('WP_CONTENT_DIR')) define('WP_CONTENT_DIR', dirname(__FILE__) . '/content');
-if (!defined('WP_CONTENT_URL')) define('WP_CONTENT_URL', WP_HOME . '/content');
