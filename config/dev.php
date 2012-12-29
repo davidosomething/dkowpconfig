b064fe69b4a1b64d0db7d1abe1bd28071c4b7c1f
David O'Trakoun <me@davidosomething.com>
2012-12-29 16:47:44 -0500

    determine ini_set string value based on

--- config/dev.php
+++ config/dev.php
@@ -10,14 +10,14 @@ define('FS_CHMOD_DIR',  02775); // setgid and ug+rw
 define('FS_CHMOD_FILE', 0775);  // ug+rw
 
 // Debugging
-$debug_on = true;
+$debug_on = true; $debug_on_string = $debug_on ? 'On' : 'Off';
 define('WP_DEBUG',          $debugon);
 define('SAVEQUERIES',       $debugon);
 define('SCRIPT_DEBUG',      $debugon);
 define('WP_DEBUG_LOG',      $debugon);
 define('WP_DEBUG_DISPLAY',  !$debugon);
-@ini_set('log_errors',      'Off');
-@ini_set('display_errors',  'Off');
+@ini_set('log_errors',      $debug_on_string);
+@ini_set('display_errors',  $debug_on_string);
 
 // Database
 define('DB_NAME',     '');
