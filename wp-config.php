4a7a3b81a531e85ffc41700cb10546d78db612f6
David O'Trakoun <me@davidosomething.com>
2013-04-02 15:50:07 -0400

    add support for and example of server specific config

--- wp-config.php
+++ wp-config.php
@@ -16,6 +16,7 @@ define('DKO_CONFIG_DIR', DKO_SITE_DIR . '/config');
 $environments = array(
   'local',
   'dev',
+  // array('dev.mysite.com' => 'dev'), // this is an example servername specific config
   'qa',
   'stage',
   'prod',
@@ -23,6 +24,18 @@ $environments = array(
 
 include DKO_CONFIG_DIR . '/common-before.php';
 foreach ($environments as $environment) {
+  // config is server specific
+  if (is_array($environment)) {
+    $key = key($environment);
+    if ($key === $_SERVER['SERVER_NAME']) {
+      $environment = $environment[$key];
+    }
+    else { // ignore this config
+      continue;
+    }
+  }
+
+  // use this config if it exists on server
   $config_file = DKO_CONFIG_DIR . '/' . $environment . '.php';
   if (file_exists($config_file)) {
     include $config_file;
