1117649072f6b3f3fff18a8ac06194d2dccd0591
David O'Trakoun <me@davidosomething.com>
2013-07-02 14:25:50 -0400

    use shorter OR syntax for defining consts, clean up dkowpmigration

--- config/common-after.php
+++ config/common-after.php
@@ -4,20 +4,29 @@
  * This file is included AFTER the environment specific config
  */
 
+defined('WPLANG')   or define('WPLANG', '');
+
+// turn on cache on prod config
+defined('WP_CACHE') or define('WP_CACHE', false);
+
+// use system cron instead
+defined('DISABLE_WP_CRON') or define('DISABLE_WP_CRON', true);
+
+// default to jetpack offline mode
+defined('JETPACK_DEV_DEBUG') or define('JETPACK_DEV_DEBUG', true);
+
 // Site URLs -- these should be manually set for Production
-if (!defined('WP_HOME')) define('WP_HOME',    DKO_CONFIG_PROTOCOL . '//' . $_SERVER['SERVER_NAME']); // path to blog root
-if (!defined('WP_SITEURL')) define('WP_SITEURL', WP_HOME . '/wp'); // path to wordpress
+defined('DKO_CORE_FOLDER') or define('DKO_CORE_FOLDER', '/wp'); // name of folder WP is in
+defined('WP_HOME')    or define('WP_HOME',    DKO_CONFIG_PROTOCOL . '//' . $_SERVER['SERVER_NAME']); // path to blog root
+defined('WP_SITEURL') or define('WP_SITEURL', WP_HOME . DKO_CORE_FOLDER); // path to wordpress
 
-if (!defined('WP_CONTENT_DIR')) define('WP_CONTENT_DIR', dirname(__DIR__) . '/content');
-if (!defined('WP_CONTENT_URL')) define('WP_CONTENT_URL', WP_HOME . '/content');
+defined('WP_CONTENT_DIR') or define('WP_CONTENT_DIR', dirname(__DIR__) . '/content');
+defined('WP_CONTENT_URL') or define('WP_CONTENT_URL', WP_HOME . '/content');
 
-if (!defined('WP_PLUGIN_DIR')) define('WP_PLUGIN_DIR', dirname(__DIR__) . '/plugins');
-if (!defined('WP_PLUGIN_URL')) define('WP_PLUGIN_URL', WP_HOME . '/plugins');
-if (!defined('PLUGINDIR')) define('PLUGINDIR', dirname(__DIR__) . '/plugins'); // compatibility
-
-// Default language
-if (!defined('WPLANG')) define('WPLANG', '');
+defined('WP_PLUGIN_DIR')  or define('WP_PLUGIN_DIR', dirname(__DIR__) . '/plugins');
+defined('WP_PLUGIN_URL')  or define('WP_PLUGIN_URL', WP_HOME . '/plugins');
+defined('PLUGINDIR')      or define('PLUGINDIR', dirname(__DIR__) . '/plugins'); // compatibility
 
 // Don't touch if WordPress lives in the wp folder
-if (!defined('ABSPATH')) define('ABSPATH', dirname(__DIR__) . '/wp/');
+defined('ABSPATH') or define('ABSPATH', dirname(__DIR__) . DKO_CORE_FOLDER . '/');
 require_once(ABSPATH . 'wp-settings.php');
--- content/mu-plugins/dkowpconfig/migration.php
+++ content/mu-plugins/dkowpconfig/migration.php
@@ -3,22 +3,25 @@ class DKOMigration
 {
   public function __construct() {
     $this->migrations_file_basename = strtolower(SERVER_ENVIRONMENT);
-    $this->migrations_file = DKO_SITE_DIR . "/migrations/options-{$this->migrations_file_basename}.php";
+    $this->migrations_file          = DKO_SITE_DIR . "/migrations/options-{$this->migrations_file_basename}.php";
 
     // do nothing if no migrations file. Filesystem checks should be superfast.
     if (!file_exists($this->migrations_file)) {
       return false;
     }
 
-    if ($_GET['dkomigration'] || $this->is_update_required()) {
-      add_action('init', array(&$this, 'update_options'));
+    $this->migrations_file_mtime    = filemtime($this->migrations_file);
+    if (!empty($_GET['dkomigration']) && $this->is_update_required()) {
+      add_action('init', array($this, 'update_options'));
+    }
+    elseif ($this->is_update_required()) {
+      add_action('admin_notices', array($this, 'notice_migration_required'));
     }
   }
 
   private function is_update_required() {
-    $database_timestamp = get_option('dkomigrate_timestamp');
-    $actual_timestamp   = filemtime($this->migrations_file);
-    return $database_timestamp != $actual_timestamp;
+    $this->last_migration = get_option('dkomigrate_timestamp');
+    return $this->last_migration != $this->migrations_file_mtime;
   }
 
   public function update_options() {
@@ -65,11 +68,11 @@ class DKOMigration
       }
     }
 
-    update_option('dkomigrate_timestamp', filemtime($this->migrations_file));
-    add_action('admin_notices', array(&$this, 'show_updated_notice'));
+    update_option('dkomigrate_timestamp', $this->migrations_file_mtime);
+    add_action('admin_notices', array($this, 'notice_migration_successful'));
   }
 
-  public function show_updated_notice() {
+  public function notice_migration_successful() {
     echo '<div id="message" class="updated fade">
   <p><strong>
     The WordPress Options table has been successfully migrated for the ', SERVER_ENVIRONMENT, ' server. Last update: ', filemtime($this->migrations_file), '
@@ -77,8 +80,13 @@ class DKOMigration
 </div>';
   }
 
-
+  public function notice_migration_required() {
+    echo '<div id="message" class="updated fade">';
+    echo '<p><strong>The DKOMigrations mu-plugin has detected that an options migration is required.</strong></p>';
+    echo '<ul>';
+    echo "  <li>{$this->migrations_file} was last updated {$this->migrations_file_mtime}</li>";
+    echo "  <li>The last migration was performed on {$this->last_migration}</li>";
+    echo '</ul><p><a href="', admin_url('/?dkomigration=true'), '">Click here to migrate your options.</a></p></div>';
+  }
 }
-if (!empty($_GET['dkomigration']) || is_admin()) {
-  new DKOMigration;
-}
+new DKOMigration;
